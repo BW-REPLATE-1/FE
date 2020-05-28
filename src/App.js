fc35e9ec6f3d52695c46a53e2ee97d4366215885
Andrew Haberman <a.haberman25@gmail.com>
2020-05-28 01:19:53 -0400

    set up dashboard correctly (working)

--- src/App.js
+++ src/App.js
@@ -1,74 +1,63 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import SignUpForm from "./components/forms/SignUp";
-import RDashboard from './components/restaurants/RDashboard';
-import VDashboard from './components/volunteers/VDashboard';
-import UpdateRestaurant from './components/forms/UpdateRestaurant';
-import UpdateVolunteer from './components/forms/UpdateVolunteer';
-import Login from './components/forms/Login';
-import PrivateRoute from './util/PrivateRoute';
+import Dashboard from "./components/dashboard/Dashboard";
+import UpdateRestaurant from "./components/forms/UpdateRestaurant";
+import UpdateVolunteer from "./components/forms/UpdateVolunteer";
+import Login from "./components/forms/Login";
+import PrivateRoute from "./util/PrivateRoute";
 
 function App() {
-  const logout = (e) => {
+	const logout = (e) => {
 		e.preventDefault();
 		localStorage.removeItem("token");
 		window.location.reload(false);
-  };
-  
+	};
 
-
-  return (
-    <div className="App">
-      <Router>
-      <h1>replate</h1>
-      {/* <Login />
-      <SignUpForm />
-      <RDashboard />
-      <VDashboard />
-      <UpdateRestaurant />
-      <UpdateVolunteer /> */}
-      <ul>
-							{localStorage.getItem("token") === null ? (
-								<h2>Login to your Dashboard</h2>
-							) : (
-								<div>
-										<div>
-											<div>
-												<Link to="/rdashboard">
-													Restaurant Dashboard
-												</Link>
-											</div>
-											<div>
-												<Link to="/vdashboard">
-													Volunteer Dashboard
-												</Link>
-                        </div>
-											<button
-												className='logout'
-												onClick={logout}>
-												logout
-											</button>
-										</div>
-								</div>
-							)}
-						</ul>
-						<Switch>
-							<PrivateRoute
-								exact
-								path="/rdashboard"
-								component={RDashboard}
-							/>
-							<PrivateRoute
-								exact
-								path="/vdashboard"
-								component={VDashboard}
-							/>
-							<Route path="/login" component={Login} />
-							<Route component={Login} />
-						</Switch>
-            </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<h1>REplate</h1>
+				<ul>
+					{localStorage.getItem("token") === null ? (
+						<>
+							<h2>Login to your Dashboard</h2>
+							<Link className="loginLink" to="/login">
+								Login
+							</Link>
+							<Link className="signupLink" to="/signup">
+								Sign Up
+							</Link>
+						</>
+					) : (
+						<div>
+							<div>
+								<button
+									className="logout"
+									onClick={logout}>
+									logout
+								</button>
+							</div>
+						</div>
+					)}
+				</ul>
+				<Switch>
+						<Route exact path="/" component={Login} />
+						<Route exact path="/login" component={Login} />
+						<Route
+							exact
+							path="/signup"
+							component={SignUpForm}
+						/>
+						<PrivateRoute
+							exact
+							path="/dashboard"
+							component={Dashboard}
+						/>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/dashboard/ReceiveJob.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+import { axiosWithAuth } from "../../util/axiosWithAuth";
+
+function ReceiveJob() {
+	const [userData, setUserData] = useState({
+		username: "iampopcorn",
+		password: "password11",
+		email: "iampopcorn@gmail.com",
+		phone_number: 3125559012,
+		isBusiness: true
+	})
+
+	// useEffect(() => {
+	// 	axiosWithAuth()
+	// 	.get('/user')
+	// 	.then(res => {
+	// 		console.log(res)
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err)
+	// 	})
+
+	// }, [])
+
+	return (
+		<div>
+			<h1>Hello {userData.username}</h1>
+			
+		</div>
+	)
+}
+
+export default ReceiveJob;
